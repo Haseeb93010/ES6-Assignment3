3d4f95a90d01fe6cc69ca1c760df526eb31aee4a
Haseeb Raza <yahooaliraza930@gmail.com>
2024-03-30 19:05:36 -0700

    create printAllCities function in script.js file

--- Js/script.js
+++ Js/script.js
@@ -1,37 +1,51 @@
 let cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Multan'];
 console.log(cities[0]);
 
+// print original string ----------------------------------------------------------- !
+for (let i = 0; i < cities.length; i++) {
+    document.getElementById("originalString").innerHTML += i + 1 + ') ' + cities[i] + '&emsp;';
+}
+
 // clear input --------------------------------------------------------------------- !
 
-function clearInput(){
-    document.getElementById("inputText").value ="";
+function clearInput() {
+    document.getElementById("inputText").value = "";
 }
 
 // clear result -------------------------------------------------------------------- !
 
-function clearResult(){
-    document.getElementById("output").innerHTML =""
+function clearResult(blank) {
+    document.getElementById("output").innerHTML = blank
 }
 
 // show output function ------------------------------------------------------------ !
 
-function showOutput(output){
+function showOutput(output) {
     document.getElementById("output").innerHTML = output;
 }
 
 // simple alert function ----------------------------------------------------------- !
 
-function simpleAlert(){
+function simpleAlert() {
     alert("I'm alert.")
 }
 
 // printMyName function ------------------------------------------------------------- !
 
-function printMyName(){
+function printMyName() {
     let myName = document.getElementById("inputText").value;
-    if(!myName){
+    if (!myName) {
         alert("Please Enter Your Name.");
         return;
     }
     showOutput(myName);
+}
+
+// print all cities function --------------------------------------------------------- ! 
+
+function printAllCities(){
+    clearResult('');
+    for (let i = 0; i < cities.length; i++) {
+        document.getElementById("output").innerHTML += i + 1 + ') ' + cities[i] + "<br />" ;
+    }
 }
