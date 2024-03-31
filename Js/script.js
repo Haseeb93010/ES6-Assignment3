b87840619414341c15d1578b81e6278445c55e19
Haseeb Raza <yahooaliraza930@gmail.com>
2024-03-30 18:42:36 -0700

    create clearInput and clearResult function in script.js file

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,12 @@
-let cities = [];
+let cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Multan'];
+console.log(cities[0]);
 
+function clearInput(){
+    document.getElementById("inputText").value ="";
+}
+function clearResult(){
+    document.getElementById("output").innerHTML =""
+}
 function simpleAlert(){
     alert("I'm alert.")
 }
