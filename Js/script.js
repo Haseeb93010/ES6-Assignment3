baf52d07018c7893134a9027c2520c2390de359b
Haseeb Raza <yahooaliraza930@gmail.com>
2024-03-30 18:55:44 -0700

    create showOutput and printMyName function in script.js file

--- Js/script.js
+++ Js/script.js
@@ -1,12 +1,37 @@
 let cities = ['Faisalabad', 'Lahore', 'Karachi', 'Islamabad', 'Multan'];
 console.log(cities[0]);
 
+// clear input --------------------------------------------------------------------- !
+
 function clearInput(){
     document.getElementById("inputText").value ="";
 }
+
+// clear result -------------------------------------------------------------------- !
+
 function clearResult(){
     document.getElementById("output").innerHTML =""
 }
+
+// show output function ------------------------------------------------------------ !
+
+function showOutput(output){
+    document.getElementById("output").innerHTML = output;
+}
+
+// simple alert function ----------------------------------------------------------- !
+
 function simpleAlert(){
     alert("I'm alert.")
+}
+
+// printMyName function ------------------------------------------------------------- !
+
+function printMyName(){
+    let myName = document.getElementById("inputText").value;
+    if(!myName){
+        alert("Please Enter Your Name.");
+        return;
+    }
+    showOutput(myName);
 }
